1f97feb1bc7adbb01c96fa2c541f5cfd2a1e2f6f
rlycano07 <baral.rushan@gmail.com>
2021-05-15 16:23:52 +1000

    initial Post template  added

--- voice/src/components/content/Content.tsx
+++ voice/src/components/content/Content.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 import './Content.css';
+import Mainbar from './mainbar/Mainbar';
+import Sidebar from './sidebar/Sidebar';
 
 export default function Content() {
-    return <div className="content"> Rendering content </div>
+    return (
+    <div className="content"> 
+        <div className="bar-wrapper">
+            <Mainbar />
+            <Sidebar />
+        </div>
+
+    </div>
+    );
 }
---
+++ voice/src/components/content/mainbar/Mainbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import CloseIcon from "@material-ui/icons/Close";
+import './Mainbar.css';
+
+export default function Mainbar(){
+    return <div className="mainbar">
+        <span className="trending-posts-title">Trending posts</span>
+
+        <div className="update-card"> 
+            <div className="top-section">
+                <span>New posts</span>
+                <CloseIcon className="hoverable"/>
+
+            </div>
+            <div className="body">
+                <span className="title">Welcome to Voice, where every word matters.</span>
+                <span className="about">This is a beta version of the Voice website where you can post discussion topics, present ideas, 
+                show support or denial while learning more about your community.  
+                </span>
+                
+            </div>
+        </div>
+    </div>
+}
